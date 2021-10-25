1b6fa5f37c943bb0bcd4c54f221d93d393fe86f7
Sergiy <ipisaryev@gmail.com>
2021-10-25 13:38:05 +0300

    add more auth tests

--- pages/login.page.js
+++ pages/login.page.js
@@ -4,7 +4,9 @@ class LoginPage extends Page {
     get inputEmail () { return $('#normal_login_email') }
     get inputPassword () { return $('#normal_login_password') }
     get buttonLogin () { return $('.login-form-button') }
-
+    get notification() { return $('.ant-notification-notice-message')}
+    get emailValidation() { return $('#normal_login .ant-form-item-has-error:nth-child(2) [role=\'alert\']')}
+    get passwordValidation() { return $('#normal_login .ant-form-item-has-error:nth-child(3) [role=\'alert\']')}
 
     async login (email, password) {
         await this.inputEmail.setValue(email);
