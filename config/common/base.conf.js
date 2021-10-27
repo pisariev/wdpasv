dcd9f26049db8526f3ee282386767109de9b39ba
pisariev <86107013+pisariev@users.noreply.github.com>
2021-10-27 14:58:49 +0300

    split conig (#5)
    * add allure reporter
    
    * add allure reporter
    
    * split config
    add firefox support
    add headless support
    
    * delete wdio.conf.js
    
    Co-authored-by: Sergiy <ipisaryev@gmail.com>

---
+++ config/common/base.conf.js
@@ -0,0 +1,28 @@
+import hooks from './hooks.conf.js';
+
+exports.config = {
+    specs: [
+        './specs/**/*.spec.js'
+    ],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://localcoding.us',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 30000,
+    connectionRetryCount: 1,
+
+    services: ['devtools', 'geckodriver'],
+
+    framework: 'mocha',
+
+    reporters: ['spec', ['allure', {
+        disableWebdriverStepsReporting: true
+    }]],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    ...hooks
+};
