b21e9dc79cd4c8c3ea36e072f78f625642fde98b
pisariev <86107013+pisariev@users.noreply.github.com>
2021-11-01 16:10:54 +0200

    More tests (#15)
    * add auth precondition
    add navigation spec
    add app pages
    
    * add auth precondition
    add navigation spec
    add app pages
    
    * add auth precondition
    add navigation spec
    add app pages
    
    Co-authored-by: Sergiy <ipisaryev@gmail.com>

--- pages/app/diary.page.js
+++ pages/app/diary.page.js
@@ -1,7 +1,7 @@
 import BasePage from './base.page';
 
 class DiaryPage extends BasePage {
-    get headerItem() { return $('//h1[contains(@class, "jumbo-title")][contains(text(), "Diaries")]') }
+    get headerItem() { return $('//h1[.=\'Daily reports\']') }
 }
 
 export default new DiaryPage();
